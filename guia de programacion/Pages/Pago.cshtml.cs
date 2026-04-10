0e69c0cf823b31bff29f899c60cf555a3e78ac29
A2016049FacioParra <pablofp.004@gmail.com>
2026-04-09 22:04:29 -0600

    Agrego de Pago.cshtml y Modificacion de Planes.cshtml

---
+++ guia de programacion/Pages/Pago.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace spotify.Pages.Premium
+{
+    // Asegúrate de que aquí diga Index1Model (con el '1')
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
